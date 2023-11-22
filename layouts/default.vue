a41db536dbea8beb9d2f26f829cb5e020c1a2757
Ali Ranjbar <info@aliranjbar.ir>
2023-11-23 00:55:36 +0330

    -

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,27 @@
-<script setup lang="ts">
-
+<script setup>
+const bodyClass = ref('')
+useListen('change-body-class', function (className) {
+  bodyClass.value = className
+})
 </script>
 
 <template>
-  $END$
+  <div :class="['min-h-screen', bodyClass]">
+    <MainMenu />
+    <NuxtPage />
+  </div>
 </template>
 
-<style scoped lang="postcss">
-
+<style>
+body {
+  background-color: #00171F;
+}
+</style>
+<style scoped>
+.red-shadow {
+  box-shadow: inset 0 300px 200px -200px #FF000050;
+}
+.yellow-shadow {
+  box-shadow: inset 0 300px 200px -200px #FDC82350;
+}
 </style>
