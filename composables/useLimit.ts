fd4a25d3cd22d96750208e265fac4e82492238d1
Ali Ranjbar <info@aliranjbar.ir>
2023-11-29 17:50:37 +0330

    scan page 1/3

--- composables/useLimit.ts
+++ composables/useLimit.ts
@@ -1,4 +1,3 @@
 export const useLimit = (str:string, limit:number = 20, addition:string = ' ...') => {
-    if (str.length <= 20) return str
-    return str.substring(0,limit) + addition
+    return str.length <= 20 ? str : str.substring(0, limit) + addition;
 }
